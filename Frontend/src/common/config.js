1e55cfc516a6f35c4c52b32c666dd817d7cdb33a
Praneetha Sripada <61249007+sp-neetha@users.noreply.github.com>
2020-12-02 13:31:20 -0800

    Update config.js

--- Frontend/src/common/config.js
+++ Frontend/src/common/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     //backendURI: 'http://localhost:3001'
-    backendURI: 'https://weather-data-forecast.herokuapp.com/'
+    backendURI: 'https://weather-data-forecast.herokuapp.com'
   };
